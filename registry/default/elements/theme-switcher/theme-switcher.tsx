434df8332511d0593ec94793a472e003c3d60e06
Railly <railly_2009@hotmail.com>
2025-08-31 01:03:24 -0500

    fix: rename theme switcher

--- registry/default/elements/theme-switcher/theme-switcher.tsx
+++ registry/default/elements/theme-switcher/theme-switcher.tsx
@@ -5,7 +5,7 @@ import { useEffect, useState } from "react";
 import { Switch } from "@/components/ui/switch";
 import { MoonIcon, SunIcon } from "lucide-react";
 
-export function ThemeSwitcherElement() {
+export function ThemeSwitcher() {
   const { theme, setTheme } = useTheme();
   const [mounted, setMounted] = useState(false);
 
